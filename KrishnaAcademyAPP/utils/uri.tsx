c372feb0fcdb652822edd4f1b464e38d686894b7
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-08-15 06:15:21 +0000

    ```text refactor: Update SERVER_URI in uri.tsx
    The SERVER_URI in uri.tsx is updated to use the "https://lms-app-1-eoim.onrender.com" endpoint instead of the "https://effective-space-cod-49xp5rgg7gphwg9-3000.app.github.dev" endpoint. This change ensures that the correct server URI is used for the KrishnaAcademyAPP.

--- KrishnaAcademyAPP/utils/uri.tsx
+++ KrishnaAcademyAPP/utils/uri.tsx
@@ -2,7 +2,7 @@
 // export const SERVER_URI = "https://lrzp6q-4000.csb.app"
 //dev
 // export const SERVER_URI = "https://lms-app-wqwt.onrender.com"
-// export const SERVER_URI = "https://lms-app-1-eoim.onrender.com"
+export const SERVER_URI = "https://lms-app-1-eoim.onrender.com"
 
-export const SERVER_URI = "https://effective-space-cod-49xp5rgg7gphwg9-3000.app.github.dev"
+// export const SERVER_URI = "https://effective-space-cod-49xp5rgg7gphwg9-3000.app.github.dev"
   // process.env.SERVER_URI || "https://bookish-fiesta-5pw7j94v67g3pvpw-4000.app.github.dev";
