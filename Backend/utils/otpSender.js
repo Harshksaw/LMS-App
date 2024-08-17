9009bb5095b33d27981595f5321de3046d20f4e5
Harsh kumar <mister.harshkumar@gmail.com>
2024-08-17 20:03:15 +0530

    chore: Update axios npm dependency to version 1.7.4

--- Backend/utils/otpSender.js
+++ Backend/utils/otpSender.js
@@ -1,4 +1,6 @@
- const sendOtp = async function sendOTP(otp, phone) {
+const axios = require('axios');
+
+const sendOtp = async function sendOTP(otp, phone) {
     console.log(`OTP for ${phone} is ${otp}`);
     // Send OTP to the phone number
     await axios.get(`https://www.fast2sms.com/dev/bulkV2?authorization=${process.env.SMSKEY}&route=otp&variables_values=${otp}&flash=1&numbers=${phone}`)
