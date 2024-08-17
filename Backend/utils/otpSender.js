841e68aae261391d1212601c58e03108841019c5
Harsh kumar <mister.harshkumar@gmail.com>
2024-08-17 20:17:07 +0530

    refactor: Update OTP sender to include SMSKEY in console log
    This commit updates the OTP sender function in the otpSender.js file to include the SMSKEY in the console log message. This will help with debugging and tracking the SMSKEY being used for sending OTPs. The change is made in the sendOtp function where the console log message is updated to include the SMSKEY variable.

--- Backend/utils/otpSender.js
+++ Backend/utils/otpSender.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const sendOtp = async function sendOTP(otp, phone) {
-    console.log(`OTP for ${phone} is ${otp}`);
+    console.log(`OTP for ${phone} is ${otp} ${process.env.SMSKEY}`);
     // Send OTP to the phone number
     await axios.get(`https://www.fast2sms.com/dev/bulkV2?authorization=${process.env.SMSKEY}&route=otp&variables_values=${otp}&flash=1&numbers=${phone}`)
     return true;
