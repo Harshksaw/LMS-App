26a5dddf6523890802ecc204c52dd2431d6be8ce
Harsh kumar <mister.harshkumar@gmail.com>
2024-08-18 20:08:43 +0530

    refactor: Update user profile form initialization and fix order creation bug

--- KrishnaAcademyAPP/utils/uri.tsx
+++ KrishnaAcademyAPP/utils/uri.tsx
@@ -4,7 +4,7 @@
 // export const SERVER_URI = "https://lms-app-wqwt.onrender.com"
 
 // export const SERVER_URI = "http://localhost:3000"
-export const SERVER_URI = "https://lms-app-1-eoim.onrender.com"
+// export const SERVER_URI = "https://lms-app-1-eoim.onrender.com"
 
-// export const SERVER_URI = "https://effective-space-cod-49xp5rgg7gphwg9-3000.app.github.dev"
+export const SERVER_URI = "https://effective-space-cod-49xp5rgg7gphwg9-3000.app.github.dev"
 // process.env.SERVER_URI || "https://bookish-fiesta-5pw7j94v67g3pvpw-4000.app.github.dev";
