4062d433a8fd62faffb99cd90671b8cb63f1d5cd
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-02 21:57:17 -0700

    Refactor file imports and remove unused code

--- AdminDashboard/src/components/core/Dashboard/AddCourse/AddCourse.jsx
+++ AdminDashboard/src/components/core/Dashboard/AddCourse/AddCourse.jsx
@@ -24,25 +24,12 @@ export default function AddCourse() {
 
       {/* Course Upload Tips */}
       <div className="sticky top-10 hidden lg:block max-w-[400px] flex-1 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6 ">
-        <p className="mb-8 text-lg text-richblack-5">⚡ Course Upload Tips</p>
+  <p className="mb-8 text-lg text-richblack-5">⚡ Course Upload Tips</p>
 
-        <ul className="ml-5 list-item list-disc space-y-4 text-xs text-richblack-5">
-          <li>Set the Course Price option or make it free.</li>
-          <li>Standard size for the course thumbnail is 1024x576.</li>
-          <li>Video section controls the course overview video.</li>
-          <li>Course Builder is where you create & organize a course.</li>
-          <li>
-            Add Topics in the Course Builder section to create lessons,quizzes,
-            and assignments.
-          </li>
-          <li>
-            Information from the Additional Data section shows up on thecourse
-            single page.
-          </li>
-          <li>Make Announcements to notify any important</li>
-          <li>Notes to all enrolled students at once.</li>
-        </ul>
-      </div>
+  <ul className="ml-5 list-item list-disc space-y-4 text-md text-richblack-5">
+    <li>Until upload is completed, don't quit. After it's uploaded, you can do something else but do look at CPU usage.</li>
+  </ul>
+</div>
     </div>
   );
 }
--- AdminDashboard/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ AdminDashboard/src/components/core/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -19,7 +19,7 @@ export default function CourseInformationForm({ step, handleNextStep, handlePrev
   const [videoPreview, setVideoPreview] = useState(null);
   const [courseId, setCourseId] = useState("");
   const [loading, setLoading] = useState(false);
-
+  const [uploadProgress, setUploadProgress] = useState(0);
   const handleVideoChange = (event) => {
     const file = event.target.files[0];
     setVideoFile(file);
@@ -66,6 +66,8 @@ export default function CourseInformationForm({ step, handleNextStep, handlePrev
   const VideoUpload = async (e) => {
     e.preventDefault();
     setLoading(true);
+    setUploadProgress(0); // Reset upload progress for video
+
     const formData = new FormData();
     formData.append("video", videoFile);
     formData.append("lessonId", courseId);
@@ -73,114 +75,139 @@ export default function CourseInformationForm({ step, handleNextStep, handlePrev
     try {
       const res = await axios.post(`${BASE_URL}/api/v1/video`, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data', // Corrected: Only 'Content-Type' header needed
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => { // Track upload progress for video
+          const totalUploadSize = progressEvent.loaded;
+          const totalFileSize = progressEvent.total;
+          const progress = Math.round((totalUploadSize / totalFileSize) * 100);
+          setUploadProgress(progress);
         },
       });
       if (res.status === 201) {
         toast.success("Video Uploaded successfully!");
         setLoading(false);
-        navigate('/dashboard/course-video'); // Replace with your desired path
+        navigate('/dashboard/all-course-video'); // Replace with your desired path
         toast.success("Redirecting to the course page...");
-
       }
     } catch (error) {
       console.error(error);
-      // toast.error("An error occurred. Please try again.");
       setLoading(false); // Reset loading state on error
     }
   };
 
   return (
     <div>
-      {loading ? (
-        <div className='flex justify-center items-center h-screen'>
-          <div className='spinner'></div>
+     {loading ? (
+        <div className='flex justify-center  h-screen'>
+          {step === 1 ? (
+            <div className="flex flex-col items-center ">
+              <div className="spinner"></div>
+              <p className="text-lg text-richblack-5 mt-4">Creating Course...</p>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center mt-5">
+              <div className="spinner"></div>
+              <p className="text-lg text-richblack-5 mt-4">Uploading Video...</p>
+              <div className="w-full bg-gray-200 rounded-full h-5 mt-2">
+                <div
+                  className="bg-blue-500 h-full rounded-full transition-all duration-500"
+                  style={{ width: `${uploadProgress}%` }}
+                ></div>
+                <p className="text-center text-2xl m-0 text-blue-500 font-bold">
+                  {uploadProgress}%
+                </p>
+              </div>
+            </div>
+          )}
         </div>
       ) : (
-        <div>
-          {/* Conditional rendering based on step */}
-          {step === 1 && (
-            <form
-              onSubmit={onSubmit}
-              className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6"
-            >
-              {/* Step 1: Course Title and Description */}
-              <div className="flex flex-col space-y-2">
-                <label className="text-sm text-richblack-5" htmlFor="courseTitle">
-                  Course Title <sup className="text-pink-200">*</sup>
-                </label>
-                <input
-                  id="courseTitle"
-                  name="courseTitle"
-                  placeholder="Enter Course Title"
-                  value={data.courseTitle}
-                  onChange={handleInputChange}
-                  className="form-style w-full"
-                />
-              </div>
-              <div className="flex flex-col space-y-2">
-                <label className="text-sm text-richblack-5" htmlFor="courseShortDesc">
-                  Course Short Description <sup className="text-pink-200">*</sup>
-                </label>
-                <textarea
-                  id="courseShortDesc"
-                  name="courseShortDesc"
-                  placeholder="Enter Description"
-                  value={data.courseShortDesc}
-                  onChange={handleInputChange}
-                  className="form-style resize-x-none min-h-[130px] w-full"
-                />
-              </div>
-              <div className="flex flex-col space-y-2">
-                <label className="text-sm text-richblack-5" htmlFor="courseImage">
-                  Course Image <sup className="text-pink-200">*</sup>
-                </label>
-                <input
-                  type="file"
-                  id="courseImage"
-                  name="courseImage"
-                  onChange={handleInputChange}
-                  className="form-style w-full"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary px-4 py-2 rounded-lg text-2xl bg-yellow-5">
-                Next
+      <div>
+        {/* Conditional rendering based on step */}
+        {step === 1 && (
+          <form
+            onSubmit={onSubmit}
+            className="space-y-8 rounded-md border-[1px] border-richblack-700 bg-richblack-800 p-6"
+          >
+            {/* Step 1: Course Title and Description */}
+            <div className="flex flex-col space-y-2">
+              <label className="text-sm text-richblack-5" htmlFor="courseTitle">
+                Course Title <sup className="text-pink-200">*</sup>
+              </label>
+              <input
+                id="courseTitle"   
+
+                name="courseTitle"
+                placeholder="Enter Course Title"
+                value={data.courseTitle}
+                onChange={handleInputChange}
+                className="form-style w-full"
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label className="text-sm text-richblack-5" htmlFor="courseShortDesc">
+                Course Short Description <sup className="text-pink-200">*</sup>
+              </label>
+              <textarea
+                id="courseShortDesc"   
+
+                name="courseShortDesc"
+                placeholder="Enter Description"
+                value={data.courseShortDesc}
+                onChange={handleInputChange}
+                className="form-style resize-x-none min-h-[130px] w-full"
+              />
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label className="text-sm text-richblack-5" htmlFor="courseImage">
+                Course Image <sup className="text-pink-200">*</sup>
+              </label>
+              <input
+                type="file"
+                id="courseImage"
+                name="courseImage"
+                onChange={handleInputChange}
+                className="form-style w-full"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary px-4 py-2 rounded-lg text-2xl bg-yellow-5">
+              Next
+            </button>
+          </form>
+        )}
+        {step === 2 && (
+          <form onSubmit={VideoUpload}>
+            <div className="flex flex-col space-y-2">
+              <label className="text-sm text-richblack-5" htmlFor="courseVideo">
+                Course Video <sup className="text-pink-200">*</sup>
+              </label>
+              <input
+                type="file"
+                id="courseVideo"
+                name="courseVideo"
+                accept="video/*"
+                onChange={handleVideoChange}
+                className="form-style w-full"
+              />
+              {videoPreview && (
+                <video controls className="mt-2 w-full">
+                  <source src={videoPreview} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              )}
+            </div>
+            <div>
+              <button type="submit" disabled={loading} className="btn btn-primary px-4 py-2 rounded-lg text-2xl bg-yellow-5">
+                {loading ? 'Submitting...' : 'Submit'}
               </button>
-            </form>
-          )}
-          {step === 2 && (
-            <form onSubmit={VideoUpload}>
-              <div className="flex flex-col space-y-2">
-                <label className="text-sm text-richblack-5" htmlFor="courseVideo">
-                  Course Video <sup className="text-pink-200">*</sup>
-                </label>
-                <input
-                  type="file"
-                  id="courseVideo"
-                  name="courseVideo"
-                  accept="video/*"
-                  onChange={handleVideoChange}
-                  className="form-style w-full"
-                />
-                {videoPreview && (
-                  <video controls className="mt-2 w-full">
-                    <source src={videoPreview} type="video/mp4" />
-                    Your browser does not support the video tag.
-                  </video>
-                )}
-              </div>
-              <div>
-                <button type="submit" disabled={loading} className="btn btn-primary px-4 py-2 rounded-lg text-2xl bg-yellow-5">
-                  {loading ? 'Submitting...' : 'Submit'}
-                </button>
-                <button type="button" onClick={handlePrevStep} className="btn btn-secondary px-4 py-2 rounded-lg text-2xl bg-gray-500 ml-4">
-                  Back
-                </button>
-              </div>
-            </form>
-          )}
-        </div>
-      )}
-    </div>
-  );
+              <button type="button" onClick={handlePrevStep} className="btn btn-secondary px-4 py-2 rounded-lg text-2xl bg-gray-500 ml-4">
+                Back
+              </button>
+            </div>
+          </form>
+        )}
+      </div>
+    )}
+  </div>
+);
 }
